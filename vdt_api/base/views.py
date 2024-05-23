9445faa88bf8c319d0c40173d531cedad56e8503
vinhbh <hoangvinh1577@gmail.com>
2024-05-23 14:41:22 +0700

    test push commit from feat-7 branch

--- vdt_api/base/views.py
+++ vdt_api/base/views.py
@@ -53,6 +53,7 @@ def getCreateStudent(request):
 
 @api_view(['DELETE'])
 def getDeleteStudent(request, id):
+    print("test delete api")
     try:
         student = Student.objects.get(id=id)
         student.delete()
